244b9fdb7163434b47a0e961ae51228b88d6989d
Keng Lee <KengLee@Hoeungs-MacBook-Pro.local>
2020-10-30 13:32:23 -0700

    t

--- src/components/apisource.js
+++ src/components/apisource.js
@@ -1,5 +1,5 @@
 export const USER_SERVER = '/api/users';
 export const API_KEY= '6d847dd445937e774fc046b365a96985'
-export const API_URL = 'http://api.themoviedb.org/3/';
+export const API_URL = 'https://api.themoviedb.org/3/';
 
 export const IMAGE_URL = 'https://image.tmdb.org/t/p/';
